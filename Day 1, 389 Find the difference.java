3ceaa62ddd6c674232c0d40acc861708183f8a78
Sarthak Dev <91304126+sarthakdev18@users.noreply.github.com>
2023-10-12 13:23:56 +0530

    Add files via upload
    Day 1 
    Problem 389
    Find the difference

---
+++ Day 1, 389 Find the difference.java
@@ -0,0 +1,16 @@
+class Solution {
+ public:
+  char findTheDifference(string s, string t) {
+    char a = 0;
+
+    for (char c : s) {
+      a ^= c;
+    }  
+
+    for (char c : t) {
+      a ^= c;
+    }
+
+    return a;
+  }
+};
