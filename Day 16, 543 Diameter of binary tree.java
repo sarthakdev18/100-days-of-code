122c70ea1ec7daee3351d5b0dd2c45b02061dd33
Sarthak Dev <91304126+sarthakdev18@users.noreply.github.com>
2023-10-18 10:31:51 +0530

    Add files via upload
    Day 16 (18/10/23)
    Problem 543
    Diameter of binary tree

---
+++ Day 16, 543 Diameter of binary tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+6
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return r;
+    }
+
+    private int r = 0;
+    int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int a = maxDepth(root.left);
+        int b = maxDepth(root.right);
+        r = Math.max(r, a + b);
+        return 1 + Math.max(a ,b);
+    }
+}
