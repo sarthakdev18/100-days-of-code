b990164672dfe1486200741d421253a7073532ac
Sarthak Dev <91304126+sarthakdev18@users.noreply.github.com>
2023-10-29 11:10:13 +0530

    Add files via upload
    Day 27 (29/10/23)
    Problem 145
     Binary Tree Postorder Traversal

---
+++ Day 27, 145 Binary Tree Postorder Traversal.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if(root == null) {
+            return new ArrayList<>();
+        }
+
+        List<Integer> post = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+
+        while(!stack.isEmpty()) {
+            root = stack.pop();
+            post.add(root.val);
+
+            if(root.left != null) {
+                stack.push(root.left);
+            }
+
+            if(root.right != null) {
+                stack.push(root.right);
+            }
+        }
+
+        Collections.reverse(post);
+        return post;
+    }
+}
