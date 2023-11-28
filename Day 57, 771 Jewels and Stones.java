2b5a98ae7c432937be21c4b9fe286366f0f47ea2
Sarthak Dev <91304126+sarthakdev18@users.noreply.github.com>
2023-11-28 13:15:08 +0530

    Add files via upload
    Day 57
    problem 771
    jewels and stones

---
+++ Day 57, 771 Jewels and Stones.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        
+        for(char j : jewels.toCharArray()) {
+            for(char s : stones.toCharArray()) {
+                if(j == s) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
